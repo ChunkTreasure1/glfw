ba99f130f2f28f5f261f347a15136bcadf7aa790
Ivar Jönsson <36239671+ChunkTreasure1@users.noreply.github.com>
2022-05-23 13:58:42 +0200

    Added files and updated premake

--- premake5.lua
+++ premake5.lua
@@ -9,13 +9,23 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+	
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
 	}
 	filter "system:linux"
 		pic "On"
@@ -49,9 +59,12 @@ project "GLFW"
 		files
 		{
 			"src/win32_init.c",
+			"src/win32_module.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+			"src/win32_time.h",
 			"src/win32_time.c",
+			"src/win32_thread.h",
 			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
@@ -65,6 +78,11 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
+		links
+		{
+			"Dwmapi.lib"
+		}
+
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
@@ -72,3 +90,8 @@ project "GLFW"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
